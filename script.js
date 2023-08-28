984f99c2538eea5fc3139e6248487aec93bc6ab7
SantRocha <128643723+SantRocha@users.noreply.github.com>
2023-08-28 12:17:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 let h2 = document.querySelector('h2');
 var map;
-var marker;
+var savedZoom = 15; // Nível de zoom inicial
+var savedCenter = [0, 0]; // Coordenadas do centro inicial
 
 function success(pos) {
     console.log(pos.coords.latitude, pos.coords.longitude);
     h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
 
+    savedCenter = [pos.coords.latitude, pos.coords.longitude]; // Salvar novas coordenadas
+
     if (map === undefined) {
-        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 15);
-
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '© OpenStreetMap'
-        }).addTo(map);
-
-        marker = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
-            .bindPopup('Localização Atual')
-            .openPopup();
+        map = L.map('mapid').setView(savedCenter, savedZoom);
     } else {
-        marker.setLatLng([pos.coords.latitude, pos.coords.longitude]); // Atualizar posição do marcador
-        map.setView([pos.coords.latitude, pos.coords.longitude], map.getZoom()); // Manter o zoom atual
+        map.remove();
+        map = L.map('mapid').setView(savedCenter, savedZoom);
     }
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    L.marker(savedCenter).addTo(map)
+        .bindPopup('Localização Atual')
+        .openPopup();
 }
 
 function error(error) {
