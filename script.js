1255f6ea2eeb76cc46b70c2ecf1e82667a7b7f45
SantRocha <128643723+SantRocha@users.noreply.github.com>
2023-08-28 12:14:14 -0500

    newlocation

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 let h2 = document.querySelector('h2');
 var map;
+var marker;
 
-function success(pos){
+function success(pos) {
     console.log(pos.coords.latitude, pos.coords.longitude);
     h2.textContent = `Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`;
-    
-    if (map === undefined){
+
+    if (map === undefined) {
         map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 15);
-    } else{
-        map.remove();
-        map = L.map('mapid').setView([pos.coords.latitude, pos.coords.longitude], 15);
-    } 
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
 
-    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
-        .bindPopup('Localozação Atual')
-        .openPopup();
-
+        marker = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
+            .bindPopup('Localização Atual')
+            .openPopup();
+    } else {
+        marker.setLatLng([pos.coords.latitude, pos.coords.longitude]); // Atualizar posição do marcador
+        map.setView([pos.coords.latitude, pos.coords.longitude], map.getZoom()); // Manter o zoom atual
+    }
 }
 
-function error(error){
+function error(error) {
     console.log(error);
-    h2.textContent = `Usuario não permitiu rastreio`
+    h2.textContent = `Usuário não permitiu rastreamento`;
 }
 
-var watchID = navigator.geolocation.watchPosition(success, error)
-
-//navigator.geolocation.clearWatch(watchID)
-
+var watchID = navigator.geolocation.watchPosition(success, error);
